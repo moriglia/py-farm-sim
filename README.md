9d4f9b4de0b699f48b86c63bde292abe74d5ec79
Marco Origlia <m.origlia@studenti.unipi.it>
2019-11-01 18:50:29 +0100

    Test for usage manager
    I reorganized the directry structure so that it contains both code and
    tests. From the root directory of the project it is possible to run
    tests as described in the README.md files by runnig the `unittest`
    module.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # py-farm-sim
+
+## Testing
+
+For all tests:
+```bash
+python3 -m unittest discover
+```
+
+For a specific test (e.g. `test/test_usage.py`)
+```bash
+python3 -m unittest test.test_usage
+```
---
+++ test/test_usage.py
@@ -0,0 +1,61 @@
+import unittest
+from pyfarmsim.usagemanager import UsageManager as UM
+from pyfarmsim.server import Server, FullQueue
+import simpy as sp
+import random
+from pyfarmsim.utils import t
+import time
+
+
+def usage_ask(env, s):
+    for i in range(20):
+        yield env.timeout(0.2)
+        u = s._usage_manager.usage_last_interval(0.5)
+        print(f"[T: {t()}] {s} CPU utilization: {u*100}%, sample {i} ")
+
+
+
+def incoming_request(idn,env, server, processing_time):
+    yield env.timeout(processing_time)# wait a bit before making request 
+
+    try:
+        print(f"[T: {t()}] Sending REQ {idn} to {server.name}")
+        yield server.request(idn, processing_time)
+        print(f"[T: {t()}] REQ {idn} successful")
+    except FullQueue as fq:
+        print(f"[T: {t()}] REQ {idn} rejected: {fq}")
+
+    return
+
+def capacity_setter(env, server):
+    for i in range(5):
+        yield env.timeout(0.5)
+        print(f"[T: {t()}] Updating capacity: {server}")
+        server.set_capacity(5+i)
+        print(f"[T: {t()}] Updated  capacity: {server}")
+
+    for i in range(5):
+        yield env.timeout(0.5)
+        print(f"[T: {t()}] Updating capacity {server}")
+        server.set_capacity(10-i)
+        print(f"[T: {t()}] Updated  capacity {server}")
+
+
+class UsageTest(unittest.TestCase):
+    def setUp(self):
+        self.e = sp.RealtimeEnvironment()
+        self.s = Server(self.e, 4, 10, "Corborant")
+        self.e.process(usage_ask(self.e,self.s))
+        self.e.process(capacity_setter(self.e,self.s))
+        for i in range(100):
+            proc_time = random.expovariate(1.5) + 0.5
+            self.e.process(incoming_request(i, self.e, self.s, proc_time))
+            print(f"Created process with ID: {i} and processing time {proc_time} units")
+
+    def test(self):
+        self.e.run()
+
+if __name__=="__main__":
+    random.seed(time.time())
+    unittest.main()
+
